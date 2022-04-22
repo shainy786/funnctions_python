a403c1d67ab253a9bb65608ac03cd8043abdc267
Shainaz <102401756+shainy786@users.noreply.github.com>
2022-04-22 12:16:23 +0530

    Create programs by using functions.py

---
+++ programs by using functions.py
@@ -0,0 +1,28 @@
+# '''adding 2 numbers by functions'''
+# def s(a,b):
+#     c=a+b
+#     d=a+b
+#     print("sum",c)
+#     print("sum",d)
+# s(3,6)
+# s(6,8)
+
+
+    # '''by using return()'''
+# def s(a,b):
+#     c=a+b
+#     return c
+
+# a=s(3,6)
+# print("sum of a is",a)
+# b=s(6,8)
+# print("sum of b is",b)
+
+# '''to find even or odd'''
+# def oe(a):
+#      if a%2==0:
+#          print(a," is even number")
+#      else:
+#          print(a,"is a odd number")
+# oe(6)
+# oe(5)
